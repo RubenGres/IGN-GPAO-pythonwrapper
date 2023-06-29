9e739ca2e3ce3b22e7b24def0e1bc1fd57fe7b8f
rgres <ruben.gres@ign.fr>
2023-06-29 10:36:05 +0200

    added conda support

--- pygpao.py
+++ pygpao.py
@@ -6,6 +6,7 @@ from gpao.builder import Builder
 from gpao.project import Project
 from gpao.job import Job
 
+CWD = os.getcwd()
 PYTHON_KERNEL_PATH = "env.db"
 
 JOBS = {}
@@ -18,6 +19,7 @@ def job(**kwargs):
     def decorator(func):
         name = kwargs.get("name", f"{func.__name__}")
         project = kwargs.get("project", os.path.basename(sys.argv[0]))
+        conda_env = kwargs.get("conda_env", None)
         tags = kwargs.get("tags", None)
         args = kwargs.get("args", {})
         deps = kwargs.get("deps", None)
@@ -27,7 +29,12 @@ def job(**kwargs):
         python_command = f'"import dill; dill.load_session(\'{session_path}\'); {func.__name__}({formatted_args})"'
         cmd = f"python3 -c {python_command}"
         cmd = cmd.replace('"', '\\"')
-        cmd = f'bash -c "cd {os.getcwd()} && {cmd}"'
+        cmd = "cd {CWD} && {cmd}"
+
+        if conda_env:
+            cmd = f'conda activate {conda_env} && {cmd}'
+
+        cmd = f'bash -c "{cmd}"'
 
         if deps:
             job_deps = [JOBS[project][dep] for dep in deps]
